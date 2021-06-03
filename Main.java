b3cad8bb1e6eefa84a3b841a9454e7261182d901
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-06-03 10:13:42 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,63 +1,73 @@
 package com.company;
 
-import java.util.Locale;
-
 public class Main {
 
-   public static void main(String[] args) {
-// 	    int value = 3;
-//	    if (value ==1) {
-//            System.out.println("Value was 1");
-//        }
-//	    else if(value == 2) {
-//            System.out.println("Value was 2");
-//        }
-//	    else {
-//            System.out.println("Was not 1 or 2");
-//        }
+    public static void main(String[] args) {
 
-	    int switchValue = 3;
+        printDayOfTheWeek(-1);
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(3);
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(6);
+        printDayOfTheWeek(7);
 
-	    switch(switchValue) {
+    }
+
+    private static void printDayOfTheWeek (int day) {
+
+        if(day == 0) {
+            System.out.println("Sunday");
+        }
+        else if (day == 1) {
+            System.out.println("Monday");
+        }
+        else if (day == 2) {
+            System.out.println("Tuesday");
+        }
+        else if (day == 3) {
+            System.out.println("Wednesday");
+        }
+        else if (day == 4) {
+            System.out.println("Thursday");
+        }
+        else if (day == 5) {
+            System.out.println("Friday");
+        }
+        else if (day == 6) {
+            System.out.println("Saturday");
+        }
+        else {
+            System.out.println("Invalid Day");
+        }
+
+        switch(day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
             case 1:
-                System.out.println("Value was 1");
+                System.out.println("Monday");
                 break;
-
             case 2:
-                System.out.println("Value was 2");
+                System.out.println("Tuesday");
                 break;
-
-            case 3: case 4: case 5:
-                System.out.println("was a 3, or a 4, or a 5");
-                System.out.println("Actually it was a " + switchValue);
+            case 3:
+                System.out.println("Wednesday");
                 break;
-
-            default:
-                System.out.println("Was not 1, 2, 3, 4 or 5");
+            case 4:
+                System.out.println("Thursday");
                 break;
-        }
-
-        char switchValueChar = 'J';
-
-	    switch (switchValueChar) {
-            case 'A': case 'B': case 'C': case 'D': case 'E':
-                System.out.println("The char value was " + switchValueChar);
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
                 break;
             default:
-                System.out.println("The char value wasn't in the list");
+                System.out.println("Invalid Day");
                 break;
-        }
-
-        String month = "January";
-	    switch (month.toLowerCase()) {
-            case "january":
-                System.out.println("January was found");
-                break;
-            case "june":
-                System.out.println("June was found");
-                break;
-            default:
-            System.out.println("Not sure");
         }
     }
 }
